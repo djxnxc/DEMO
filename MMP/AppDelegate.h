1d66b3c57e2fcda38cd74ad292f34fbe9ee8dde1
12 <1402017233@qq.com>
2017-11-02 16:15:50 +0800

    新建项目

---
+++ MMP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MMP
+//
+//  Created by 12 on 17/11/2.
+//  Copyright © 2017年 12. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MMP/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MMP
+//
+//  Created by 12 on 17/11/2.
+//  Copyright © 2017年 12. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
